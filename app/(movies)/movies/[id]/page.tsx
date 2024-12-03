2f1ab4ebbf037de34ef85178cd880289a8415857
eunseopi <asxzqw12@naver.com>
2024-12-03 14:12:01 +0900

    movie commit

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,19 @@
 import MovieVideos from "../../../../components/movie-videos";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, {getMovie} from "../../../../components/movie-info";
 import {Suspense} from "react";
 
-export default async function MovieDetail({
-                                              params: {id},
-                                          } : {
-    params: {id: string}
-}) {
+interface IParams {
+    params: { id: string };
+}
+
+export async function generateMetadata({params: {id}} : IParams){
+    const movie = await getMovie(id);
+    return {
+        title: movie.title,
+    }
+}
+
+export default async function MovieDetailPage({params: {id},} : IParams) {
     return <div>
         <Suspense fallback={<h1>Loading movie info</h1>}>
             <MovieInfo id={id}/>
